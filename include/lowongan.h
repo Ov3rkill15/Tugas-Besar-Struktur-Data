a384caeb89a81443e8ee7311b63123259a5dbf75
zhaturra1 <alfarizifathur12@gmail.com>
2025-12-13 19:00:16 +0700

    Update lowongan.h
    header mencari lowongan berdasarkan ID lowongan

--- include/lowongan.h
+++ include/lowongan.h
@@ -8,6 +8,7 @@ void createListParent(ListParent &L);
 address_parent alokasiParent(int ID, string Posisi, string Perusahaan, float Kuota);
 void insertParent(ListParent &L, address_parent P_Baru);
 address_parent findParent(ListParent L, int ID_Target);
+void menuCariLowongan(ListParent L);
 void showLowongan(ListParent L_Parent);
 void showMahasiswa(ListChild L_Child);
 // --- FUNGSI INPUT UTAMA (INTERAKTIF) ---
